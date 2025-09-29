6fd5b7a7dbdb4ff10793ebd4cd5da181b2b830db
Mudassaralired <117730111+Mudassaralired@users.noreply.github.com>
2025-09-29 04:04:27 -0700

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,10 @@
-/** @type {import('tailwindcss').Config */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
   theme: {
     extend: {
       colors: {
-        primary: '#4F46E5', // indigo-600
-        secondary: '#10B981', // emerald-500
+        primary: '#4F46E5',
         whatsapp: '#25D366'
-      },
-      fontFamily: {
-        sans: ['Inter', 'system-ui', 'sans-serif']
       }
     },
   },
