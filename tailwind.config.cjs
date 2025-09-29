bd14b86c876e8d3eee06acf4c36184a6204f9666
Mudassaralired <117730111+Mudassaralired@users.noreply.github.com>
2025-09-29 01:04:47 -0700

    Add files via upload

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#8B5CF6',
+        whatsapp: '#25D366'
+      }
+    },
+  },
+  plugins: [],
+}
